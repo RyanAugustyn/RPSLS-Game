9849a9b27ed8252f1cd9ee5467be8532cc802960
Ryan Augustyn <jraugustyn07@gmail.com>
2023-02-21 16:29:53 -0600

    feat: added functions to print greeting and game rules

--- game.py
+++ game.py
@@ -1,3 +1,24 @@
 class Game: 
     def __init__(self) -> None:
         pass
+
+
+
+    #greeting
+    def print_greeting(self):
+        print("\nWelcome to Rock Paper Scissors...Lizard Spock!\n")
+
+
+    #print out the rules 
+    def print_instructions(self):
+        print("The rules of the game are as follows:")
+        print("Players take turns choose from one of five options,")
+        print("Rock, Paper, Scissors, Lizard, Spock. Once both options are chosen")
+        print("the game determines who wins the round based on the rules below. It ")
+        print("is best out of three rounds, so first player to two wins wins overall.")
+        print("\nThe rules for all of the options are as follows:\n")
+        print("Rock beats Scissors, loses to Paper and Spock")
+        print("Scissors beats Paper and Lizard, loses to Rock and Spock")
+        print("Paper beats Rock and Spock, loses to Scissors and Lizard")
+        print("Lizard beats Spock and Paper, loses to Rock and Scissors")
+        print("Spock beats Scissors and Rock, loses to Lizard and Paper")
