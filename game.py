6f9bb6dd8fbe8762001381256498c82c4fe46202
Ethan Doering <ethanwdoering@icloud.com>
2023-02-21 15:08:15 -0700

    feat: Added print_list that prints a list in this format. 1.item

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 class Game: 
-    def __init__(self) -> None:
+    def __init__(self):
+        self.rpsls_list = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.single_player = False
+
+    def run_game():
         pass
+
+    def print_list(self, rpsls_list):
+        index = 1
+
+        print("")
+
+        for item in rpsls_list:
+            print(f"{index}. {item}")
+            index += 1
