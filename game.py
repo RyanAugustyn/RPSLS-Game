d63276dd65264016bc3d6a35a75438acd29f7b85
Ryan Augustyn <jraugustyn07@gmail.com>
2023-02-22 16:13:25 -0600

    refactor: add run_rounds() and Gestures class

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from human_player import Human_Player
 from ai_player import AI_Player
+from gestures import Gestures
 
 class Game: 
     def __init__(self):
@@ -7,36 +8,22 @@ class Game:
         self.single_player = False
         self.player_1 = Human_Player()
         self.player_2 = AI_Player()
+        self.gesture = Gestures()
+        self.rounds = 0
 
 # Runs the RPSLS game
     def run_game(self):
         self.print_greeting()
         self.print_instructions()
         self.single_player = self.game_mode_determiner()
-        rounds = self.ask_rounds()
+        self.rounds = self.ask_rounds()
         if self.single_player == False:
             self.player_2 = Human_Player()
-        while self.player_1.round_wins < rounds and self.player_2.round_wins < rounds:
-            self.print_list(self.rpsls_list)
-            player_choice_1 = self.player_1.get_gesture(self.rpsls_list)
-            player_choice_2 = self.player_2.get_gesture(self.rpsls_list)
-            while player_choice_1 == player_choice_2:
-                print("Tie! No points awarded")
-                player_choice_1 = self.player_1.get_gesture(self.rpsls_list)
-                player_choice_2 = self.player_2.get_gesture(self.rpsls_list)
-            player_1_win = self.gesture_comparison(player_choice_1, player_choice_2)
-            self.who_won_round(player_1_win)
-        if self.player_1.round_wins == rounds:
-            print("\n\nPlayer 1 wins the game!!!")
-        else:
-            print("\n\nPlayer 2 wins the game!!!")
-
-
-
+        self.run_rounds()
+       
     #greeting
     def print_greeting(self):
         print("\nWelcome to Rock Paper Scissors...Lizard Spock!\n")
-
 
     #print out the rules 
     def print_instructions(self):
@@ -60,8 +47,6 @@ class Game:
     def game_mode_determiner(self):
         user_input = ""
         single_player = False
-
-
         while single_player == False:
             user_input = input("\nPlease enter in Multiplayer or Single Player for game mode: ")
             if user_input == "single player":
@@ -73,48 +58,31 @@ class Game:
 
         return single_player
 
-#ask user for number of rounds
+#Ask user for number of rounds
     def ask_rounds(self):
-        user_input = input("Please choose the number of rounds needed to win:\n")
-        
+        user_input = input("Please choose the number of rounds needed to win:\n")    
         while user_input.isnumeric() == False:
             print("Incorrect input")
             user_input = input("Please choose the number of rounds needed to win:\n")
 
-
         return int(user_input)
 
-# Performs game logic according to RPSLS rules
-    def gesture_comparison(self, choice_1, choice_2):
-        choice_1_wins = False
-
-        if choice_1 == "rock":
-            if choice_2 == "paper" or choice_2 == "spock":
-                choice_1_wins = False
-            else:
-                choice_1_wins = True
-        elif choice_1 == "scissors":
-            if choice_2 == "rock" or choice_2 == "spock":
-                choice_1_wins = False
-            else:
-                choice_1_wins = True
-        elif choice_1 == "paper":
-            if choice_2 == "scissors" or choice_2 == "lizard":
-                choice_1_wins = False
-            else:
-                choice_1_wins = True
-        elif choice_1 == "lizard":
-            if choice_2 == "rock" or choice_2 == "scissors":
-                choice_1_wins = False
-            else:
-                choice_1_wins = True
-        elif choice_1 == "spock":
-            if choice_2 == "lizard" or choice_2 == "paper":
-                choice_1_wins = False
-            else:
-                choice_1_wins = True
-
-        return choice_1_wins
+#Runs the round
+    def run_rounds(self):
+        while self.player_1.round_wins < self.rounds and self.player_2.round_wins < self.rounds:
+            self.print_list(self.rpsls_list)
+            player_choice_1 = self.player_1.get_gesture(self.rpsls_list)
+            player_choice_2 = self.player_2.get_gesture(self.rpsls_list)
+            while player_choice_1 == player_choice_2:
+                print("Tie! No points awarded")
+                player_choice_1 = self.player_1.get_gesture(self.rpsls_list)
+                player_choice_2 = self.player_2.get_gesture(self.rpsls_list)
+            player_1_win = self.gesture.gesture_comparison(player_choice_1, player_choice_2)
+            self.who_won_round(player_1_win)
+        if self.player_1.round_wins == self.rounds:
+            print("\n\nPlayer 1 wins the game!!!")
+        else:
+            print("\n\nPlayer 2 wins the game!!!")
     
 # Tells user if player_1 or player_2 won
     def who_won_round(self, is_winner):
--- gestures.py
+++ gestures.py
@@ -1,8 +1,42 @@
 class Gestures():
-    def __init__(self, gesture) -> None:
+    def __init__(self) -> None:
         pass
 
 #use the 'in' function 
 #maybe inheritance 
 #final result should be 2-3 lines in game.py
 
+
+
+
+# Performs game logic according to RPSLS rules
+    def gesture_comparison(self, choice_1, choice_2):
+        choice_1_wins = False
+
+        if choice_1 == "rock":
+            if choice_2 == "paper" or choice_2 == "spock":
+                choice_1_wins = False
+            else:
+                choice_1_wins = True
+        elif choice_1 == "scissors":
+            if choice_2 == "rock" or choice_2 == "spock":
+                choice_1_wins = False
+            else:
+                choice_1_wins = True
+        elif choice_1 == "paper":
+            if choice_2 == "scissors" or choice_2 == "lizard":
+                choice_1_wins = False
+            else:
+                choice_1_wins = True
+        elif choice_1 == "lizard":
+            if choice_2 == "rock" or choice_2 == "scissors":
+                choice_1_wins = False
+            else:
+                choice_1_wins = True
+        elif choice_1 == "spock":
+            if choice_2 == "lizard" or choice_2 == "paper":
+                choice_1_wins = False
+            else:
+                choice_1_wins = True
+
+        return choice_1_wins
